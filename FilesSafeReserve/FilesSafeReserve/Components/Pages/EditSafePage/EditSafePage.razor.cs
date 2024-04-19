53fc1fd95eeb0aff3d83b91d19c5df910fa26d6c
Bohdan Shyryi <bohdan.shyryi@gmail.com>
2024-04-19 20:15:09 +0300

    FSR-004: Created virtual safe editing page
    Changes:
    * Created EditSafePage page
    * Created ViewModel EditSafeViewModel

--- FilesSafeReserve/FilesSafeReserve/Components/Pages/EditSafePage/EditSafePage.razor.cs
+++ FilesSafeReserve/FilesSafeReserve/Components/Pages/EditSafePage/EditSafePage.razor.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FilesSafeReserve.Data.ViewModels;
+using Microsoft.AspNetCore.Components;
 
 namespace FilesSafeReserve.Components.Pages.EditSafePage;
 public partial class EditSafePage : ComponentBase
 {
     [Parameter]
     public required string Id { get; set; }
+
+    public EditSafeViewModel ViewModel { get; set; } = new();
 }
